9a82391ca0b6396200bcd5b88e87be86e37fcb07
tobyatgithub <toby.fangyuan@gmail.com>
2024-08-05 23:24:50 -0700

    Init 20-20-20 project. Have the basic Timer with interface working.

---
+++ src/Timer.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+interface TimerProps {
+  initialTime: number;
+}
+
+const Timer: React.FC<TimerProps> = ({ initialTime }) => {
+  const [time, setTime] = useState(initialTime);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if (isActive && time > 0) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (time === 0) {
+      setIsActive(false);
+      if (interval) clearInterval(interval);
+      // TODO: Trigger notification
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isActive, time]);
+
+  const toggleTimer = () => {
+    setIsActive(!isActive);
+  };
+
+  const resetTimer = () => {
+    setTime(initialTime);
+    setIsActive(false);
+  };
+
+  const formatTime = (seconds: number): string => {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+  };
+
+  return (
+    <div>
+      <h1>{formatTime(time)}</h1>
+      <button onClick={toggleTimer}>{isActive ? 'Pause' : 'Start'}</button>
+      <button onClick={resetTimer}>Reset</button>
+    </div>
+  );
+};
+
+export default Timer;
